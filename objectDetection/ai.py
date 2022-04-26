5cd5bd75b28b5693d02b456717cc71a9031cf63b
Resilliance <Jkesguerra2050@yahoo.com>
2022-04-26 16:00:23 -0400

    Attempt to implement API

---
+++ objectDetection/ai.py
@@ -0,0 +1,8 @@
+import requests
+
+
+requests.post(
+    "https://3af31b1a-06af-46fb-8445-72f7347c17b2.syndic.ai",
+    headers={"Content-Type": "application/json"},
+    data="{"url": "https://cdn.discordapp.com/attachments/405169684699283468/957660126641270794/IMG_2958.JPG"}",
+)
--- objectDetection/views.py
+++ objectDetection/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
+#import requests
+import json
+import base64
 
 # Create your views here.
 def index(request):
+    """if request.method == "POST":
+        user_input = request.POST.get('user_input', None)
+        user_url = {'user_input' : user_input }
+        if user_input is not None:
+            detection(user_url)
+        else:
+            return redirect('index')"""
     return render(request, 'wisteria/index.html')
+
+def detection(request):
+    """input_base64 = requests.post(
+                "https://3af31b1a-06af-46fb-8445-72f7347c17b2.syndic.ai",
+                headers={"Content-Type": "application/json"},
+                data= user_url)
+    decoded_input = base64.decodestring(input_base64)
+    image_result = open(decoded_input)
+    print(decoded_input)"""
+    
+    return render(request,'wisteria/detection.html')
